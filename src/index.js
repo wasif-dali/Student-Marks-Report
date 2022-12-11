09418e799f6f197e9b83c20d499a587d0b8b5397
Wasif Ali <you@example.com>
2022-12-11 22:29:29 +0530

    index file with out cluster

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-const express = require("express");
-
+const express = require('express');
+const route = require('./route/routes.js');
+const mongoose = require('mongoose');
 const app = express();
- app.all("ping",ping,(req,res)=>{
-    res.json("Ping")
- })
+mongoose.set('strictQuery', false);
 
- app.post('')
+app.use(express.json());
+mongoose.connect("", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+app.use('/', route)
+
+app.use("/*", function (req, res) {
+    return res.status(400).send({
+        status: false, message: "Make Sure Your Endpoint is Correct !!!"
+    })
+})
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
