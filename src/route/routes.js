e4c490062e3a3c061a013c16abbe89cd3ea9b47a
Wasif Ali <you@example.com>
2022-12-14 19:36:59 +0530

    route

--- src/route/routes.js
+++ src/route/routes.js
@@ -1,14 +1,22 @@
-const express = require('express');
-const router = express.Router();
-const { registration, userlogin } = require("../Controller/UserController")
-const { createstudent,getstudent,findstudent,updatestudent, deletestudent } = require("../Controller/studentController")
-const { authentication, authorization } = require("../middleware/auth")
+const express = require("express")
+const router = express.Router()
 
-router.post("/register", registration)
-router.post("/login", userlogin)
-router.post("/createstudent/:userid", authentication, authorization, createstudent)
-router.get("/getstudent/:userid",getstudent)
-router.get("/getstudent",findstudent)
-router.put("/updatestudent/:userid/:studentid", authentication, authorization, updatestudent)
-router.delete("/deletestudent/:userid/:studentid", authentication, authorization, deletestudent)
-module.exports = router;
+const { createUser, loginUser } = require("../controllers/userController")
+const {createStudent,getStudentlist,updateStudent,deleteStudent} = require("../controllers/studentController.js")
+const {authentication,authorization} = require("../middlewares/auth.js")
+
+
+
+router.get("/test", function (req, res) {
+    res.send("testing successfull")
+})
+
+router.post("/registerUser", createUser)
+router.post("/loginUser", loginUser)
+
+router.post("/registerStudent",authentication,createStudent)
+router.get("/getstudentlist",authentication,getStudentlist)
+router.put("/updateStudent/:studentId",authentication,authorization,updateStudent)
+router.delete("/deleteStudent/:studentId",authentication,authorization,deleteStudent)
+
+module.exports = router
